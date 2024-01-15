21f9e43d17dc71aa5ec4125c7bdffcd81833ec9f
Tanvir Modhu <happy2024.tat@gmail.com>
2024-01-16 03:22:53 +0600

    jani nna
    working

---
+++ recursion.c
@@ -0,0 +1,13 @@
+// finding fibonacci numbers
+// 0,1,1,2,3,5,8,13...
+#include <stdio.h>
+int fibonacci(int x)
+{
+    for (int i = 0; i <= x; i++)
+    {
+    }
+}
+int main()
+{
+    printf(fibonacci(10));
+}
